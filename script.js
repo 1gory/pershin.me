cff398778e6060d3eda68bf409b79f7a4591f82f
Igor Pershin <igor.pershin.me@gmail.com>
2023-02-11 00:07:16 +0300

    save old fixex

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  $.get("./projects.json", function (projects) {
+    projects.forEach((project, i) => {
+      const card = $('.d-none.card-template').clone().removeClass('d-none');
+      card.find('.card-title').text(project.name);
+      card.find('.card-text').text(project.description);
+      card.find('.project-link').attr('href', project.url);
+      if (project.github) {
+        card.find('.project-github').attr('href', project.github);
+      } else {
+        card.find('.project-github').remove();
+      }
+      card.find('.card-img-top').attr('src', './img/projects/' + project.img);
+      card.appendTo('.cards');
+      const technologiesP = card.find('.technologies');
+
+      project.technologies.forEach((technology, i) => {
+        const t = '<span class="badge bg-light text-dark">' + technology + '</span>';
+        $(t).appendTo(technologiesP);
+      });
+    });
+  });
+});
