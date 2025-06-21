aaa3339503a06ec8eac8f44de38b1d2cb45c6e9b
Igor Pershin <igor.pershin.me@gmail.com>
2025-06-21 15:45:05 +0300

    Add small fixes

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 $(document).ready(function () {
   $.get("./projects.json", function (projects) {
     projects.forEach((project, i) => {
-      const card = $('.d-none.card-template').clone().removeClass('d-none');
+      const template = $('.card-template')[0];
+      const card = $(template.content.cloneNode(true));
+
       card.find('.card-title').text(project.name);
       card.find('.card-text').text(project.description);
-      card.find('.project-link').attr('href', project.url);
+
+      if (project.url) {
+        card.find('.project-link').attr('href', project.url);
+      } else {
+        card.find('.project-link').remove(); // Скрыть кнопку
+      }
+
       if (project.github) {
         card.find('.project-github').attr('href', project.github);
       } else {
         card.find('.project-github').remove();
       }
-      card.find('.card-img-top').attr('src', './img/projects/' + project.img);
-      card.appendTo('.cards');
-      const technologiesText = card.find('.technologies');
 
+      card.find('.card-img-top').attr('src', './img/projects/' + project.img);
+
+      const technologiesText = card.find('.technologies');
       project.technologies.forEach((technology, i) => {
         const t = '<span class="badge bg-light text-dark">' + technology + '</span>';
         $(t).appendTo(technologiesText);
       });
+
+      $('.cards').append(card);
+
+      document.getElementById('current-year').textContent = new Date().getFullYear();
     });
   });
 });
